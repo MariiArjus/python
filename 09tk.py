6280ad8b93f5a8809476aa83f28fe2f6179bddad
MariiArjus <marii@hkhk.edu.ee>
2024-11-06 13:54:50 +0200

    Add files via upload

---
+++ 09tk.py
@@ -0,0 +1,15 @@
+
+import turtle
+#6
+d = 100
+for i in range(4):
+    turtle.rt(90)
+
+for i in range(2):
+    turtle.fd(d)
+    turtle.lt(90)
+    turtle.fd(d/2)
+    turtle.lt(90)
+
+
+turtle.done()
