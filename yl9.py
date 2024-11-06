0606af6bb030ac6ca2e11ddc2bdfb7796a5697f8
MariiArjus <marii@hkhk.edu.ee>
2024-11-06 13:58:22 +0200

    Add files via upload

--- yl9.py
+++ yl9.py
@@ -30,68 +30,177 @@ print(f"Paaris arvuse summa: {sum(paaris)} ")
 print(f"Paaritute arvude summa: {sum(paaritud)}")
 """
 
-# Kuva arvud 1-42
-# Arvud, mis jagunevad 3, lisa tekst TIK (näiteks 3 TIK)
-# Arvud, mis jagunevad 5, lisa tekst TAK (näiteks 5 TAK)
-# Kui jagunevad mõlemaga, siis lisa tekst TIKTAK (näiteks 15 TIKTAK)
+# # Kuva arvud 1-42
+# # Arvud, mis jagunevad 3, lisa tekst TIK (näiteks 3 TIK)
+# # Arvud, mis jagunevad 5, lisa tekst TAK (näiteks 5 TAK)
+# # Kui jagunevad mõlemaga, siis lisa tekst TIKTAK (näiteks 15 TIKTAK)
 
-for i in range (1,43):
-    if 1%3==0 and i%5==0:
-        print(i,"tiktak", end=" ")
-    elif i%3==0:
-        print(i,"TIK", end=" ")
-    elif i%5==0:
-        print(i,"TAK", end=" ")
-    else: 
-        print(i, end=" ")
+# for i in range (1,43):
+#     if 1%3==0 and i%5==0:
+#         print(i,"tiktak", end=" ")
+#     elif i%3==0:
+#         print(i,"TIK", end=" ")
+#     elif i%5==0:
+#         print(i,"TAK", end=" ")
+#     else: 
+#         print(i, end=" ")
 
-print()
+# print()
+"""
 # Leia kõik arvud vahemikus 200 kuni 320, mis jaguvad 7-ga, kuid ei jagu 5-ga. Prindi need arvud komadega eraldatult ühele reale. 
 
 for i in range (200,321):
     if i%7==0 and i%5>0:
         print(i, end=" ,")
+"""
+
+# # Kuva nimekirjast unikaalsed nimed:
+# nimed = ['Martin', 'Tõnu', 'Andres', 'Tõnu', 'Andres', 'Andres', 'Andres', 'Tõnu', 'Marko', 'Mari', 'Jüri', 'Liis', 'Marko', 'Piret', 'Anu']
+# unikaalsed_nimed = []
+
+# for nimi in nimed:
+#     if nimi not in unikaalsed_nimed:
+#         unikaalsed_nimed.append(nimi)
 
 
 
-
+# for nimi in unikaalsed_nimed:
+#     print(nimi)
 
 
 
 # random.randint(min, max)
 
-# Leia kõik arvud vahemikus 200 kuni 320, mis jaguvad 7-ga, kuid ei jagu 5-ga. Prindi need arvud komadega eraldatult ühele reale.
-# Kuva nimekirjast unikaalsed nimed:
-# nimed = ['Martin', 'Tõnu', 'Andres', 'Tõnu', 'Andres', 'Andres', 'Andres', 'Tõnu', 'Marko', 'Mari', 'Jüri', 'Liis', 'Marko', 'Piret', 'Anu']
-# Sulle on saadetud õpilaste keskmised hinded, mille lisasid loendisse. Eralda hinded ning leia kogu rühma parim ja kehvem tulemus ning keskmine hinne.
+
+# # Sulle on saadetud õpilaste keskmised hinded, mille lisasid loendisse. Eralda hinded ning leia kogu rühma parim ja kehvem tulemus ning keskmine hinne.
 # ryhma_hinded = ["Mari 4.9", "Jüri 3.1", "Kadri 4.6", "Marko 4.7", "Liis 4.9", "Andres 4.2", "Anu 4.7", "Martin 4.2", "Piret 4.2", "Tõnu 4.1"]
-# Koosta programm, mis genereerib ja kuvab korrutustabeli, kus iga number korrutatakse iseendaga:
+# hinded = []
+
+# for i in ryhma_hinded:
+#     hinded.append(float(i.split(" ")[1]))
+
+# print(f"Parim tulemus {max(hinded)}")
+# print(f"Halvim tulemus {min(hinded)}")
+# print(f"Keskmine tulemus {sum(hinded)/len(hinded)}")
+
+
+
+"""
+# # Koosta programm, mis genereerib ja kuvab korrutustabeli, kus iga number korrutatakse iseendaga:
 # Näiteks:
 # 0 x 0 = 0
 # 1 x 1 = 1
 # 2 x 2 = 4
-# 3 x 3 = 9
-# 4 x 4 = 16
-# 5 x 5 = 25
-# 6 x 6 = 36
-# 7 x 7 = 49
-# 8 x 8 = 64
-# 9 x 9 = 81
-# 10 x 10 = 100
+
+for i in range(11):
+    print(i)
+    print (f"{i} x {i} = {i*i}")
+"""
+
+
+
+
+
+"""
 # Loo programm, mis loob suvalised tehted 1-100 arvudega.
 # Kasuta tsükli puhul alakriipsu
 # kasuta suvalise tehte märgi jaoks loendit ja sealt suvalise märgi leidmiseks random.choice()
 # Näiteks:
 # 7 – 2=
 # 45 * 69=
-# 71 – 45=
-# 84 / 57=
-# 59 * 87=
-# 84 – 71=
-# 65 * 32=
-# 63 – 11=
-# 72 – 90=
-# 29 / 93=
+
+punktid = 0
+tehted= ['+','-','*','/']
+for _ in range(11):
+    i = random.randint(1,10)
+    j = random.randint(1,10)
+    tehe = random.choice(tehted)
+    if tehe=="+":
+        print (f"{i} {tehe} {j} = ")
+        vastus= int(input("Vastus: "))
+        if vastus== i+j:
+            print("Õige")
+            punktid+=1
+        else:
+            print("Vale vastus")
+    elif tehe=="-":
+        print (f"{i} {tehe} {j} = ")
+        vastus= int(input("Vastus: "))
+        if vastus== i-j:
+            print("Õige")
+            punktid+=1
+        else: 
+            print("Vale vastus")
+    
+    elif tehe=="*":
+        print (f"{i} {tehe} {j} = {i*j}")
+        vastus= int(input("Vastus: "))
+        if vastus== i*j:
+            print("Õige")
+            punktid+=1
+        else: 
+            print("Vale vastus")
+    else:
+        print (f"{i} {tehe} {j} = ")
+        vastus= float(input("Vastus: "))
+        if vastus== i/j:
+            print("Õige")
+            punktid+=1
+        else: 
+            print("Vale vastus")
+"""
+
+
+"""
 # Täienda eelmist ülesannet ja kasutaja käest küsitakse vastust.
 # Õiged vastused loetakse kokku
 # # Kui saab vähemalt poole punktid, siis saab A, muul juhul MA
+
+for i in range (1,6):
+    print('*'*i)
+
+for i in range (1,6):
+    print('*'*(5-i))
+
+
+for i in range (1,6):
+    print(' '*(4-i)+'*'*i)
+
+
+for i in range (1,6):
+    print(' '*(5-i) + '*' * i)
+"""
+
+
+# #13
+
+# ev_data = [
+# ['vehicle', 'range', 'price'],
+# ['Tesla Model Y Long Range', '330', '58990'],
+# ['Volkswagen ID.4 Pro', '260', '39995'],
+# ['Ford Mustang Mach-E', '300', '42995'],
+# ['Audi e-tron GT', '238', '102700'],
+# ['Nissan Leaf', '149', '27400'],
+# ['BMW iX xDrive50', '324', '83995'],
+# ['Polestar 2', '265', '45500'],
+# ['Kia EV6 Long Range', '310', '47795'],
+# ['Mercedes-Benz EQS 450+', '350', '102310'],
+# ['Hyundai Kona Electric', '258', '37400']
+# ]
+
+# keskmineOdo= []
+# keskmineHind=[]
+
+# for i in ev_data:
+#     if i[0] != "vehicle":
+#         keskmineOdo.append(int(i[1]))
+#         keskmineHind.append(int(i[2]))
+#         #print(i[1])
+#     if i[0] != "vehicle" and int(i[1]) < 300:
+#         print(i[1][0])
+
+
+# print(sum(keskmineOdo)/len(keskmineOdo))
+# print(sum(keskmineHind)/len(keskmineHind))
+
+
