8b75a414a699619ae63d5a4c83bde7dcab220ee7
MariiArjus <marii@hkhk.edu.ee>
2024-11-08 09:22:29 +0200

    Add files via upload

--- 09tk.py
+++ 09tk.py
@@ -1,15 +1,56 @@
 
 import turtle
+
 #6
 d = 100
 for i in range(4):
-    turtle.rt(90)
+    turtle.lt(90)
 
-for i in range(2):
+for i in range(4):
     turtle.fd(d)
     turtle.lt(90)
     turtle.fd(d/2)
     turtle.lt(90)
+
+for i in range(2):
+    turtle.fd(d)
+    turtle.rt(90)
+
+
+# #7
+# d= 100
+# for i in range(8):
+#     turtle.rt(360/8)
+#     turtle.fd(d/2)
+
+
+# for i in range(2):
+#     turtle.fd(d)
+#     turtle.rt(90)
+#     turtle.fd(25)
+#     turtle.rt(90)
+# turtle.lt(90)
+# turtle.fd(d/2)
+
+
+
+
+
+
+# #4
+# d = 100
+
+# for i in range(5):
+#         turtle.rt(72)
+#         turtle.fd(d/2)
+# for i in range(3):
+#     turtle.lt(90)
+#     turtle.fd(d)
+# turtle.lt(90)
+# turtle.fd(d/2)
+
+
+
 
 
 turtle.done()
