3ecf0d70581d5ce275d0daee3da6fc5654f14a41
MariiArjus <marii@hkhk.edu.ee>
2025-01-08 13:49:32 +0200

    Add files via upload

---
+++ coke.py
@@ -0,0 +1,22 @@
+# ÜLESANNE 9 - Coca-Cola automaat
+
+# Oletame, et automaat müüb Coca-Cola pudeleid hinnaga 50 senti ning aktsepteerib ainult järgmisi mündiväärtusi:
+#     25 senti
+#     10 senti
+#     5 senti
+
+# Failis coke.py loo programm, mis:
+#     Küsib kasutajalt korduvalt münti, teatades iga kord, kui palju raha on veel puudu.
+#     Kui kasutaja on sisestanud vähemalt 50 senti, arvutab programm ja väljastab tagasiantava raha hulga sentides.
+#     Eeldatakse, et kasutaja sisestab alati täisarve. Kui sisestatud väärtus ei ole 25, 10 ega 5, ignoreerib programm seda ja küsib uut sisendit.
+
+
+raha = int(input("Sisestage oma summa: "))
+hind= 50
+kassa = 0
+
+kassa + raha = 50
+# if 
+
+
+
