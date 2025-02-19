f47ef4a850f74d8d6780c941e284f2139e5871c3
MariiArjus <marii@hkhk.edu.ee>
2025-02-19 17:14:59 +0200

    Add files via upload

---
+++ kontrolltoo.py
@@ -0,0 +1,18 @@
+temp = [3,-20,1,6,3,1,-19,4,0,-16,-5,-9,-3,-19,-16,-2,-5,-3,-12,-13,3,-15,-18,-11,8,-16,5,9,-5,-8,-11]
+
+keskmine = sum(temp) / len(temp)
+
+massiiv = temp[6]
+
+if keskmine > 0:
+    print("Õues on soe")
+else:
+    print("Õues on külm")
+
+
+
+print(keskmine)
+print(massiiv)
+# tee massiiviks 
+# keskmine temp
+# kui kesk temp alla nulli on külm kuu kui on üle = soe
